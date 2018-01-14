4f5cde86b7580f6b590d342af42fa1a9bc83df25
chhenkehao1 <2601516357@qq.com>
2018-01-14 23:42:18 +0800

    初始化
    更新片库操作

--- login/spiders/__init__.py
+++ login/spiders/__init__.py
@@ -4,10 +4,16 @@
 # your spiders.
 
 import linkspider
+import os
 
 if __name__ == '__main__':
-    st = input('是否更新影视库,请输入是与否：')
+    st = input('如果要更新影视库,请输入‘是’：')
     if st == '是':
         linkspider.oa()
-
+    ku = linkspider.jianso()
+    name = input('请输入要查询的电视剧名：')
+    url = ku.get(name)
+    print(url)
+    if url != None:
+        os.makedirs(os.path.join('D:/AuI18N/', name))
 
